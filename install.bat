60ab66c9cc704c275edda5988c29b9035fd2e448
António Santos <38131791+antoniomsantos99@users.noreply.github.com>
2019-05-18 00:21:29 +0100

    Fixed errors in scripts

--- install.bat
+++ install.bat
@@ -2,5 +2,5 @@ git clone https://github.com/charliewolf/pynder.git
 cd pynder
 git fetch origin +refs/pull/211/merge
 git checkout -qf FETCH_HEAD
-python3 -m pip install --force-reinstall --no-deps .
-python3 -m pip install -r requirements.txt
+python -m pip install --force-reinstall --no-deps .
+python -m pip install -r .\requirements.txt
--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-python3 Valentine.py
+python .\Valentine.py
